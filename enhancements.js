3ee0ef01df0d2ee183d5fb116fc456fc0aa80dfb
maxlp33 <minecraft911005@gmail.com>
2025-11-27 22:21:57 +0800

    change the reveal animation trigger point and smoothness

--- enhancements.js
+++ enhancements.js
@@ -3,8 +3,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Scroll Reveal Animation
     const observerOptions = {
-        threshold: 0.1,
-        rootMargin: '0px 0px -50px 0px'
+        threshold: 0.15,
+        rootMargin: '0px 0px -20px 0px'
     };
 
     const observer = new IntersectionObserver((entries) => {
